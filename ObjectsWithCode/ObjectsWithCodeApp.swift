e030a186cd1aba25e94ce985e70f4eba9a95840d
Nuri TIRAŞ <nuritiras@gmail.com>
2024-07-20 11:41:31 +0300

    Initial Commit

---
+++ ObjectsWithCode/ObjectsWithCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectsWithCodeApp.swift
+//  ObjectsWithCode
+//
+//  Created by Nuri TIRAŞ on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ObjectsWithCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
